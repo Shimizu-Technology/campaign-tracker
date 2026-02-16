f2f81ca805b656b75dde46ae5f4b974010d682db
Jerry <jerry.shimizutechnology@gmail.com>
2026-02-16 12:14:36 +1000

    CT-43: Fix migration version to match Rails 8.1

--- api/db/migrate/20260216014500_add_foreign_key_to_sms_blasts.rb
+++ api/db/migrate/20260216014500_add_foreign_key_to_sms_blasts.rb
@@ -1,4 +1,4 @@
-class AddForeignKeyToSmsBlasts < ActiveRecord::Migration[8.0]
+class AddForeignKeyToSmsBlasts < ActiveRecord::Migration[8.1]
   def change
     add_foreign_key :sms_blasts, :users, column: :initiated_by_user_id
   end
